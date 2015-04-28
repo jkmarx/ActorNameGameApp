7ef113f56ae1459e26deac73f15ea5b7c7750bfd
J.Marx <jennyrutledge@gmail.com>
2015-04-27 23:18:11 -0400

    Add flash messages for correct and wrong answers.

--- app/scripts/controllers/GameCtrl.js
+++ app/scripts/controllers/GameCtrl.js
@@ -9,7 +9,15 @@ function GameCtrl(ActorsFactory, MoviesFactory, $location, $scope, $timeout){
   var vm = this;
   vm.counter = 3;
   vm.score = 0;
+  vm.actors = ActorsFactory.actors;
   vm.actorsNames = ActorsFactory.actorsNames;
+
+  $scope.list1 = [];
+  angular.forEach(vm.actors, function(val, key) {
+    $scope.list1.push({});
+  });
+  $scope.list2 = ActorsFactory.actorsNamesRand;
+  $scope.alerts = [];
 
   vm.searchMovie = function(movie){
     MoviesFactory.requestMovies(movie).then(function(response){
@@ -21,30 +29,35 @@ function GameCtrl(ActorsFactory, MoviesFactory, $location, $scope, $timeout){
     vm.score = vm.score + vm.counter * 10;
   }
 
+  vm.counterTally = function(){
+    if(vm.counter > 0){
+      vm.counter = vm.counter - 1;
+    }
+  }
+
   vm.checkWinner = function(){
     if(vm.checkGuess()){
       console.log("you win");
       vm.scoreTally();
       vm.counter = 3;
-      $scope.list1=[];
+      vm.addAlertWin();
+      vm.resetLists();
     }else{
       console.log("try again");
-      if(vm.counter > 0){
-        vm.counter = vm.counter - 1;
-      }
-      $scope.list1 =[];
-      angular.copy(ActorsFactory.getCastNamesRand(vm.actorsNames.slice(0)), $scope.list2);
+      vm.counterTally();
+      vm.addAlertFail();
+      vm.resetLists();
+      vm.resetRandList();
     }
   }
 
-  $scope.list1 = [];
-  vm.actors = ActorsFactory.actors;
-  angular.forEach(vm.actors, function(val, key) {
-    $scope.list1.push({});
-  });
+  vm.resetLists = function(){
+    $scope.list1 =[];
+  };
 
-
-  $scope.list2 = ActorsFactory.actorsNamesRand;
+  vm.resetRandList() = function(){
+    angular.copy(ActorsFactory.getCastNamesRand(vm.actorsNames.slice(0)), $scope.list2);
+  }
 
   vm.checkGuess = function(){
     var flag = true;
@@ -56,17 +69,28 @@ function GameCtrl(ActorsFactory, MoviesFactory, $location, $scope, $timeout){
     return flag;
   }
 
+  vm.listNames = function(arrCast){
+    var names = arrCast.map(function(actor){
+      return actor.name;
+    })
+    return names.join(', ');
+  }
+
+  vm.addAlertWin = function() {
+    $scope.alerts.push({type: 'success', msg: 'Well Done Mate! ' + vm.listNames($scope.list1) + ' was correct.'});
+  };
+
+  vm.addAlertFail = function() {
+    $scope.alerts.push({type: 'warning', msg: 'Try Again. ' + vm.listNames($scope.list1) + ' was incorrect.' });
+  }
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
   $scope.startCallback = function(event, ui, name) {
     console.log('You started draggin: ' + name);
     $scope.draggedName = name;
-  };
-
-  $scope.stopCallback = function(event, ui) {
-    console.log('Why did you stop draggin me?');
-  };
-
-  $scope.dragCallback = function(event, ui) {
-    console.log('hey, look I`m flying');
   };
 
   $scope.dropCallback = function(event, ui) {
@@ -76,11 +100,4 @@ function GameCtrl(ActorsFactory, MoviesFactory, $location, $scope, $timeout){
       }
   };
 
-  $scope.overCallback = function(event, ui) {
-    console.log('Look, I`m over you');
-  };
-
-  $scope.outCallback = function(event, ui) {
-    console.log('I`m not, hehe');
-  };
 };
